59746919d4ffe4be9fb3dbb192a9f3686835d18b
wannabesuperteur <iwant_optimi@naver.com>
2024-02-05 21:38:06 +0900

    add: loading train and test data

--- Natural Language Processing/Project 001. 영어 문장 감성 분석/run_train.py
+++ Natural Language Processing/Project 001. 영어 문장 감성 분석/run_train.py
@@ -1,3 +1,22 @@
 import pandas as pd
 import numpy as np
 import math
+
+from preprocess_data import read_data, add_age_of_user_numeric_col
+
+
+def load_data():
+    train_data = read_data('train.csv')
+    test_data = read_data('test.csv')
+
+    add_age_of_user_numeric_col(train_data)
+    add_age_of_user_numeric_col(test_data)
+
+    return train_data, test_data
+
+
+if __name__ == '__main__':
+    train_data, test_data = load_data()
+
+    print(train_data)
+    print(test_data)
