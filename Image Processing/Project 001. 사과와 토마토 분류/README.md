5cbdaa960a960fbae0b71cc51763baf5d2c8c0fd
wannabesuperteur <iwant_optimi@naver.com>
2024-02-21 00:41:13 +0900

    add: augmentation applying + flip

--- Image Processing/Project 001. 사과와 토마토 분류/README.md
+++ Image Processing/Project 001. 사과와 토마토 분류/README.md
@@ -13,16 +13,28 @@ Project 001. 사과와 토마토 분류
     - tomatoes
 ```
 
-* 수행 기간: 2024.02.14 ~ 2024.02.18 (5일)
+* 수행 기간: 2024.02.14 ~ 2024.02.22 (9일)
 
 ## 파일 및 코드 설명
 * ```augment_data.py``` : 이미지 데이터 augmentation 코드
+  * 출력 파일
+    * ```images/train/apples_X.png``` : ```apples``` 클래스의 학습 데이터 (원본)
+    * ```images/train/apples_X_hf.png``` : ```apples``` 클래스의 학습 데이터 (```horizontal flip``` 적용)
+    * ```images/train/apples_X_vf.png``` : ```apples``` 클래스의 학습 데이터 (```vertical flip``` 적용)
+    * ```images/train/apples_X_af.png``` : ```apples``` 클래스의 학습 데이터 (```horizontal + vertical flip``` 적용)
+    * ```tomatoes``` 클래스에 대해서도 ```apples```를 ```tomatoes```로 바꿔서 동일하게 생각하면 됨
 * ```train.py``` : 모델 학습
 * ```test.py``` : 모델 테스트 및 성능지표 결과 확인
 * ```main.py``` : 데이터 전처리, 모델 학습, 테스트 등 전 과정을 한번에 진행
 
 ## 데이터 전처리 과정
 * 이미지 데이터가 수백 장 이하로 적으므로, augmentation을 먼저 실시한다.
+
+## 실제 학습 및 테스트 데이터
+* 학습 데이터
+  * ```augment_data.py``` 실행 후, ```images/train/``` 폴더 내부의 데이터
+* 테스트 데이터
+  * ```archive/test/apples```, ```archive/test/tomatoes``` 폴더 내부의 데이터 (원본 데이터)
 
 ## 머신러닝 모델 설명
 
@@ -50,7 +62,7 @@ python main.py
 ## branch info
 |branch|status|type|start|end|description|
 |---|---|---|---|---|---|
-|IP-P1-master|||240214|240218|마스터 브랜치|
+|IP-P1-master|||240214|240222|마스터 브랜치|
 |IP-P1-1|```ing```|```feat```|240215||이미지 데이터 augmentation|
 |IP-P1-2||```feat```|||모델 학습|
 |IP-P1-3||```feat```|||모델 테스트 및 성능지표 결과 출력|
--- Image Processing/Project 001. 사과와 토마토 분류/augment_data.py
+++ Image Processing/Project 001. 사과와 토마토 분류/augment_data.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import numpy as np
+import shutil
 
 RESIZE_DEST = 128
 
@@ -18,11 +19,63 @@ def load_imgs(is_train, img_class):
         image_resized = cv2.resize(image, dsize=(RESIZE_DEST, RESIZE_DEST))
         images.append(image_resized)
         
-    return np.array(images) / 255.0
+    return np.array(images)
+
+
+# 각 Augmentation 방법 실시
+def flip_horizontal(image):
+    return cv2.flip(image, 1)
+
+
+def flip_vertical(image):
+    return cv2.flip(image, 0)
+
+
+def flip_all(image):
+    return cv2.flip(cv2.flip(image, 0), 1)
+
+
+def make_dir_and_imwrite(image_path, image):
+    dir_path = '/'.join(image_path.split('/')[:2])
+    try:
+        os.makedirs(dir_path)
+    except:
+        pass # "파일이 이미 있으므로 만들 수 없습니다"
+        
+    cv2.imwrite(image_path, image)
+
+
+# 각 이미지에 대해 augmentation 적용
+def apply_augmentation(images, dir_name, prefix):
+    for idx, image in enumerate(images):
+        make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}.png', image)
+
+        # horizontal flip
+        h_flipped = flip_horizontal(image)
+
+        # vertical filp
+        v_flipped = flip_vertical(image)
+
+        # both flip
+        all_flipped = flip_all(image)
+
+        make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_hf.png', h_flipped)
+        make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_vf.png', v_flipped)
+        make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_af.png', all_flipped)
+
+
+# 모든 이미지들에 대해 augmentation 적용하는 함수
+def apply_augmentation_all():
+    shutil.rmtree('images')
+    apply_augmentation(images=train_tomatoes, dir_name='train', prefix='tomatoes')
+    apply_augmentation(images=train_apples, dir_name='train', prefix='apples')
 
 
 if __name__ == '__main__':
     train_tomatoes = load_imgs(is_train=True, img_class='tomatoes')
+    train_apples = load_imgs(is_train=True, img_class='apples')
 
     print(train_tomatoes)
     print(np.shape(train_tomatoes))
+
+    apply_augmentation_all()
