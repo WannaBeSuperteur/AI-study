22e3e6bc718d22e3574aa9af8c54461d72b80cdf
wannabesuperteur <iwant_optimi@naver.com>
2024-02-21 21:03:04 +0900

    add: flip + crop augmentation

--- Image Processing/Project 001. 사과와 토마토 분류/README.md
+++ Image Processing/Project 001. 사과와 토마토 분류/README.md
@@ -1,16 +1,16 @@
 # Image Processing Project 001. 사과와 토마토 분류
 * Dataset: [Apple Tomato Classification](https://www.kaggle.com/datasets/samuelcortinhas/apples-or-tomatoes-image-classification)
-  * 해당 위치에서 다운받은 ```archive.zip``` 파일의 압축을 풀어서 다음과 같은 파일 데이터 구조를 만든다.
+  * 해당 위치에서 다운받은 데이터 파일 중 ```archive.zip``` 파일의 압축을 풀어서 다음과 같은 파일 데이터 구조를 만든다.
 
 ```
 Project 001. 사과와 토마토 분류
 - archive
   - test
-    - apples
-    - tomatoes
+    - apples (54)
+    - tomatoes (43)
   - train
-    - apples
-    - tomatoes
+    - apples (164)
+    - tomatoes (130)
 ```
 
 * 수행 기간: 2024.02.14 ~ 2024.02.22 (9일)
@@ -22,9 +22,18 @@ Project 001. 사과와 토마토 분류
     * ```images/train/apples_X_hf.png``` : ```apples``` 클래스의 학습 데이터 (```horizontal flip``` 적용)
     * ```images/train/apples_X_vf.png``` : ```apples``` 클래스의 학습 데이터 (```vertical flip``` 적용)
     * ```images/train/apples_X_af.png``` : ```apples``` 클래스의 학습 데이터 (```horizontal + vertical flip``` 적용)
+    * ```images/train/apples_X_hsv_Y.png``` : ```apples``` 클래스의 학습 데이터 (```Y```번째 ```hsv change``` 옵션 적용, 총 26가지)
+    * ```images/train/apples_X_crop_Y.png``` : ```apples``` 클래스의 학습 데이터 (```Y```번째 ```crop``` 옵션 적용, 총 15가지)
     * ```tomatoes``` 클래스에 대해서도 ```apples```를 ```tomatoes```로 바꿔서 동일하게 생각하면 됨
+  * 원본 학습 데이터 총 **294장** -> augmentation 이후 **13,230장** 으로 **45배 증대**
 * ```train.py``` : 모델 학습
+  * 학습 데이터
+    * 파일 이름이 ```images/train/apples_X``` (X = ```0, 1, ..., 131```), ```images/train/tomatoes_X``` (X = ```0, 1, ..., 103```) 로 시작하는 이미지들
+  * validation 데이터
+    * 파일 이름이 ```images/train/apples_X``` (X = ```132, 133, ..., 164```), ```images/train/tomatoes_X``` (X = ```104, 105, ..., 129```) 로 시작하는 이미지들
 * ```test.py``` : 모델 테스트 및 성능지표 결과 확인
+  * ```archive/test/apples``` 내부의 데이터
+  * ```archive/test/tomatoes``` 내부의 데이터
 * ```main.py``` : 데이터 augmentation, 데이터 전처리, 모델 학습, 테스트 등 전 과정을 한번에 진행
 
 ## 데이터 전처리 과정
--- Image Processing/Project 001. 사과와 토마토 분류/augment_data.py
+++ Image Processing/Project 001. 사과와 토마토 분류/augment_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import shutil
 
 RESIZE_DEST = 128
+CROP_SIZE = 16
 
 
 # cv2로 이미지 로딩
@@ -35,6 +36,27 @@ def flip_all(image):
     return cv2.flip(cv2.flip(image, 0), 1)
 
 
+def change_hsv(image, hc=0, sc=0, vc=0):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h = hsv[:, :, 0]
+    s = hsv[:, :, 1]
+    v = hsv[:, :, 2]
+
+    h_new = cv2.add(h, hc)
+    s_new = cv2.add(s, sc)
+    v_new = cv2.add(v, vc)
+    hsv_new = cv2.merge([h_new, s_new, v_new])
+
+    return cv2.cvtColor(hsv_new, cv2.COLOR_HSV2BGR)
+
+
+def crop(image, top=0, bottom=0, left=0, right=0):
+    height = len(image)
+    width = len(image[0])
+    
+    return image[top : height-bottom, left : width-right]
+
+
 def make_dir_and_imwrite(image_path, image):
     dir_path = '/'.join(image_path.split('/')[:2])
     try:
@@ -48,20 +70,52 @@ def make_dir_and_imwrite(image_path, image):
 # 각 이미지에 대해 augmentation 적용
 def apply_augmentation(images, dir_name, prefix):
     for idx, image in enumerate(images):
+        if idx % 25 == 0:
+            print(idx)
+            
         make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}.png', image)
 
-        # horizontal flip
+        # horizontal flip (+1 image)
         h_flipped = flip_horizontal(image)
 
-        # vertical filp
+        # vertical filp (+1 image)
         v_flipped = flip_vertical(image)
 
-        # both flip
+        # both flip (+1 image)
         all_flipped = flip_all(image)
 
         make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_hf.png', h_flipped)
         make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_vf.png', v_flipped)
         make_dir_and_imwrite(f'images/{dir_name}/{prefix}_{idx}_af.png', all_flipped)
+
+        # change hsv (+26 images)
+        current_hsv_option = 0
+        
+        for hc in [-4, 0, 4]: # hue
+            for sc in [-55, -30, 5]: # saturation
+                for vc in [-20, 0, 60]: # value
+                    if hc != 0 or sc != 5 or vc != 0:
+                        hsv_changed_img = change_hsv(image, hc, sc, vc)
+                        make_dir_and_imwrite(
+                            f'images/{dir_name}/{prefix}_{idx}_hsv_{current_hsv_option}.png',
+                            hsv_changed_img
+                        )
+                        current_hsv_option += 1
+
+        # crop (+15 images)
+        current_crop_option = 0
+
+        for top in [0, CROP_SIZE]:
+            for bottom in [0, CROP_SIZE]:
+                for left in [0, CROP_SIZE]:
+                    for right in [0, CROP_SIZE]:
+                        if top + bottom + left + right > 0:
+                            cropped_img = crop(image, top, bottom, left, right)
+                            make_dir_and_imwrite(
+                                f'images/{dir_name}/{prefix}_{idx}_crop_{current_crop_option}.png',
+                                cropped_img
+                            )
+                            current_crop_option += 1
 
 
 # 모든 이미지들에 대해 augmentation 적용하는 함수
