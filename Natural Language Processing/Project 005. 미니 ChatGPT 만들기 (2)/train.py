6d1aed6b960086a21fb134940476983c1abca3a3
wannabesuperteur <iwant_optimi@naver.com>
2024-03-01 12:22:53 +0900

    feat: training mini chatgpt model

--- Natural Language Processing/Project 005. 미니 ChatGPT 만들기 (2)/train.py
+++ Natural Language Processing/Project 005. 미니 ChatGPT 만들기 (2)/train.py
@@ -1,21 +1,93 @@
-from embedding_helper import get_token_ids
+from embedding_helper import get_token_ids, encode_one_hot
 import pandas as pd
 import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras import layers, optimizers
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.layers import Dense, LSTM, Embedding, LeakyReLU
 
 
 INPUT_TOKEN_CNT = 24 # 학습 데이터 row 당 입력 토큰 개수
+VOCAB_SIZE = len(get_token_ids()) # vocab의 크기
+print(f'VOCAB SIZE = {VOCAB_SIZE}')
+
+
+# mini chatgpt model (=NLP model)
+# ref: https://www.kaggle.com/code/carlosaguayo/predicting-the-next-word-using-lstm/notebook
+class MiniChatGPTModel(tf.keras.Model):
+    
+    def __init__(self, dropout_rate=0.25):
+        super().__init__()
+
+        L2 = tf.keras.regularizers.l2(0.001)
+
+        self.embedding = Embedding(VOCAB_SIZE, INPUT_TOKEN_CNT, input_length=INPUT_TOKEN_CNT)
+        self.LSTM_0 = LSTM(64, return_sequences=True)
+        self.LSTM_1 = LSTM(64)
+        self.dense = Dense(256, activation=LeakyReLU(alpha=0.1))
+        self.final = Dense(VOCAB_SIZE, activation='softmax')
+
+    def call(self, inputs, training):
+        embed = self.embedding(inputs)
+        intermediate_0 = self.LSTM_0(embed)
+        intermediate_1 = self.LSTM_1(intermediate_0)
+        intermediate_2 = self.dense(intermediate_1)
+        
+        outputs = self.final(intermediate_2)
+        return outputs
+
+
+# 모델 반환
+def define_model():
+    optimizer = optimizers.Adam(0.001, decay=1e-6)
+    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=5)
+    lr_reduced = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=2)
+        
+    model = MiniChatGPTModel()
+    return model, optimizer, early_stopping, lr_reduced
+
+
+# input data, output data 로부터 train, valid data 추출
+def define_data(input_data_all, output_data_all, valid_ratio=0.1):
+    
+    valid_cnt = int(valid_ratio * len(input_data_all))
+
+    # define train and valid, input and output data
+    train_input_data = input_data_all[:-valid_cnt]
+    train_output_data = output_data_all[:-valid_cnt]
+
+    valid_input_data = input_data_all[-valid_cnt:]
+    valid_output_data = output_data_all[-valid_cnt:]
+
+    return train_input_data, train_output_data, valid_input_data, valid_output_data
+
 
 
 # NLP 모델 학습
 def train_model(input_data_all, output_data_all):
-    print(f'input : {np.shape(input_data_all)}\n{input_data_all}\n')
-    print(f'output : {np.shape(output_data_all)}\n{output_data_all}\n')
+    (train_input, train_output, valid_input, valid_output) = define_data(input_data_all, output_data_all)
 
-    # TODO
+    print(f'train input : {np.shape(train_input)}\n{train_input}\n')
+    print(f'valid input : {np.shape(valid_input)}\n{valid_input}\n')
+    print(f'train output : {np.shape(train_output)}\n{train_output}\n')
+    print(f'valid output : {np.shape(valid_output)}\n{valid_output}\n')
+    
+    model, optimizer, early_stopping, lr_reduced = define_model()
+    
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+    model.fit(
+        train_input, train_output,
+        callbacks=[early_stopping, lr_reduced],
+        epochs=50,
+        validation_data=(valid_input, valid_output)
+    )
+
+    model.summary()
+    model.save('mini_chatgpt_model')
+
+    return model
 
 
 # train_data.csv 파일의 데이터 전체를 token id로 만들어서 반환
@@ -74,7 +146,7 @@ def get_train_data_token_ids(token_ids, verbose=False, limit=None):
 
         for input_token in input_tokens:
             input_text_token_ids.append(token_ids[input_token])
-        output_text_token_ids.append(token_ids[output_token])
+        output_text_token_ids.append(encode_one_hot(token_ids=token_ids, token=output_token))
 
         train_data_iter_cnt += 1
 
@@ -99,7 +171,7 @@ def run_all_process(limit=None):
 
 # NLP 모델 테스트
 def test_model(text, is_return=False):
-    main_model = tf.keras.models.load_model('main_model')
+    main_model = tf.keras.models.load_model('mini_chatgpt_model')
 
     # TODO
 
