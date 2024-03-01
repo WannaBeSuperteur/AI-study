a1ad55fbc82b79a4caeb1df2ba2603769af46d70
wannabesuperteur <iwant_optimi@naver.com>
2024-03-01 11:39:24 +0900

    feat: finish generating train data

--- Natural Language Processing/Project 005. 미니 ChatGPT 만들기 (2)/generate_data.py
+++ Natural Language Processing/Project 005. 미니 ChatGPT 만들기 (2)/generate_data.py
@@ -3,22 +3,51 @@ import pandas as pd
 from tokenize_data import tokenize_file_content
 
 
-INPUT_SIZE = 16
+INPUT_SIZE = 24
 
 
 # 학습 데이터 생성
-def generate_data(tokens):
+def generate_data(tokens, verbose=False):
     df = pd.DataFrame()
 
     for i in range(len(tokens) - INPUT_SIZE):
-        data_row = ' '.join(tokens[i:i + INPUT_SIZE + 1])
+        if i % 500 == 0 and verbose:
+            print(i)
+            
+        data_input = tokens[i:i + INPUT_SIZE + 1]
+        data_row = ' '.join(data_input)
+
+        # 새로운 row 추가
         new_row = {'data': [data_row]}
         new_row = pd.DataFrame(new_row)
         df = pd.concat([df, new_row])
+
+        # 입력 데이터 중 <Person-Change> 가 있으면 그 전의 모든 token을 <Null> 로 교체한 새로운 row 추가
+        # 실제로는 입력 데이터의 최초 20개 token 중 <Person-Change> 가 있어야 함
+        data_input_first_20 = tokens[i + 1:i + INPUT_SIZE - 4]
+        
+        if '<Person-Change>' in data_input_first_20:
+            data_row_with_null = []
+            is_person_change_detected = False
+            
+            for j in range(INPUT_SIZE, -1, -1):
+                if is_person_change_detected:
+                    data_row_with_null = ['<Null>'] + data_row_with_null
+                else:
+                    data_row_with_null = [data_input[j]] + data_row_with_null
+                    if data_input[j] == '<Person-Change>':
+                        is_person_change_detected = True
+
+            data_row_with_null = ' '.join(data_row_with_null)
+
+            # <Null> 을 포함한 새로운 row 추가
+            new_row_with_null = {'data': [data_row_with_null]}
+            new_row_with_null = pd.DataFrame(new_row_with_null)
+            df = pd.concat([df, new_row_with_null])
 
     df.to_csv('train_data.csv')
     
 
 if __name__ == '__main__':
     tokens = tokenize_file_content('archive/human_chat.txt')
-    generate_data(tokens)
+    generate_data(tokens, verbose=True)
