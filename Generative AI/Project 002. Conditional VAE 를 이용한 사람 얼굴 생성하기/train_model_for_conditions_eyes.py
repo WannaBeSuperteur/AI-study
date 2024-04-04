cca1f4b7ef31f64efb34b4c2aecac7d832a417de
wannabesuperteur <iwant_optimi@naver.com>
2024-04-04 21:25:37 +0900

    feat: smaller input area from resized image, for eyes model

--- Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/train_model_for_conditions_eyes.py
+++ Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/train_model_for_conditions_eyes.py
@@ -9,8 +9,9 @@ import pandas as pd
 
 from helper_regression_models import load_first_1k, read_output, read_gender_prob
 
-IMG_SIZE = 60
 ORIGINAL_IMG_SIZE = 120
+IMG_WIDTH = 60
+IMG_HEIGHT = IMG_WIDTH // 2 + 2
 
 
 class Regression_Eyes_Model(tf.keras.Model):
@@ -24,18 +25,20 @@ class Regression_Eyes_Model(tf.keras.Model):
         L2 = tf.keras.regularizers.l2(0.001)
 
         # conv + pooling part
-        # 60 -> 58 -> 56 -> 28 -> 26 -> 13 -> 11 -> 9
-        self.conv_0 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=[IMG_SIZE, IMG_SIZE, 3])
-        self.conv_1 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+        # 60 -> 58 -> 56 -> 28 -> 26 -> 13 -> 11 -> 9 (horizontal)
+        # 32 -> 30 -> 28 -> 14 -> 12 ->  6 ->  4 -> 2 (vertical)
+        
+        self.conv_0 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=[IMG_HEIGHT, IMG_WIDTH, 3])
+        self.conv_1 = tf.keras.layers.Conv2D(48, (3, 3), activation='relu')
         self.conv_2 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
-        self.conv_3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
-        self.conv_4 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
+        self.conv_3 = tf.keras.layers.Conv2D(96, (3, 3), activation='relu')
+        self.conv_4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')
 
         # fully connected part
-        self.dense_0 = tf.keras.layers.Dense(256, activation=tf.keras.layers.LeakyReLU(alpha=0.06),
+        self.dense_0 = tf.keras.layers.Dense(320, activation=tf.keras.layers.LeakyReLU(alpha=0.06),
                                            kernel_regularizer=L2, name='dense_0')
 
-        self.dense_1 = tf.keras.layers.Dense(64, activation=tf.keras.layers.LeakyReLU(alpha=0.06),
+        self.dense_1 = tf.keras.layers.Dense(120, activation=tf.keras.layers.LeakyReLU(alpha=0.06),
                                            kernel_regularizer=L2, name='dense_1')
 
         self.final_dense = tf.keras.layers.Dense(1, activation='sigmoid',
@@ -43,19 +46,21 @@ class Regression_Eyes_Model(tf.keras.Model):
 
 
     def call(self, inputs, training):
-        inputs_img, inputs_gender = tf.split(inputs, [IMG_SIZE * IMG_SIZE * 3, 2], axis=1)
-        inputs_img = tf.keras.layers.Reshape((IMG_SIZE, IMG_SIZE, 3))(inputs_img)
+        inputs_img, inputs_gender = tf.split(inputs, [IMG_HEIGHT * IMG_WIDTH * 3, 2], axis=1)
+        inputs_img = tf.keras.layers.Reshape((IMG_HEIGHT, IMG_WIDTH, 3))(inputs_img)
 
-        # conv + pooling part : 60 -> 58 -> 56 -> 28 -> 26 -> 13 -> 11 -> 9
-        outputs_0 = self.conv_0(inputs_img) # 58
-        outputs_1 = self.conv_1(outputs_0) # 56
-        outputs_2 = self.pooling(outputs_1) # 28
+        # conv + pooling part : 60 -> 58 -> 56 -> 28 -> 26 -> 13 -> 11 -> 9 (horizontal)
+        #                       32 -> 30 -> 28 -> 14 -> 12 ->  6 ->  4 -> 2 (vertical)
+        
+        outputs_0 = self.conv_0(inputs_img) # horizontal 58 / vertical 30
+        outputs_1 = self.conv_1(outputs_0) # 56 / 28
+        outputs_2 = self.pooling(outputs_1) # 28 / 14
 
-        outputs_3 = self.conv_2(outputs_2) # 26
-        outputs_4 = self.pooling(outputs_3) # 13
+        outputs_3 = self.conv_2(outputs_2) # 26 / 12
+        outputs_4 = self.pooling(outputs_3) # 13 / 6
 
-        outputs_5 = self.conv_3(outputs_4) # 11
-        outputs_6 = self.conv_4(outputs_5) # 9
+        outputs_5 = self.conv_3(outputs_4) # 11 / 4
+        outputs_6 = self.conv_4(outputs_5) # 9 / 2
         outputs_flatten = self.flatten(outputs_6)
 
         # fully connected part
@@ -73,8 +78,8 @@ class Regression_Eyes_Model(tf.keras.Model):
 # 모델 반환
 def define_model():
     optimizer = optimizers.Adam(0.001, decay=1e-6)
-    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=5)
-    lr_reduced = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=2)
+    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=7)
+    lr_reduced = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=3)
         
     model = Regression_Eyes_Model()
     return model, optimizer, early_stopping, lr_reduced
@@ -89,7 +94,7 @@ def train_cnn_model(train_input_img, train_input_gender_prob, train_output):
     print(f'train input (gender prob) : {np.shape(train_input_gender_prob)}\n{train_input_gender_prob}\n')
     print(f'train output              : {np.shape(train_output)}\n{train_output}\n')
 
-    train_input_img_ = np.reshape(train_input_img, (-1, IMG_SIZE * IMG_SIZE * 3))
+    train_input_img_ = np.reshape(train_input_img, (-1, IMG_HEIGHT * IMG_WIDTH * 3))
     train_input_gender_prob_ = np.reshape(train_input_gender_prob, (-1, 2))
     train_input = np.concatenate((train_input_img_, train_input_gender_prob_), axis=1)
 
@@ -123,16 +128,16 @@ def get_gender_prob_data(male_img_names, female_img_names):
 
 
 # 이미지를 가로 4등분, 세로 4등분하여 16등분했을 때,
-# 가로로 위쪽에서 2, 3번째 줄과 세로로 위쪽에서 1, 2번째 줄의 교차점 (16등분 중 총 4개 영역) 에 해당하는 부분만 있는 이미지로 crop
+# 가로로 위쪽에서 2, 3번째 줄과 세로로 위쪽에서 2번째 줄의 교차점 (16등분 중 총 2개 영역) 에 해당하는 부분만 있는 이미지로 crop
 def crop_images(all_images):
     img_count = len(all_images)
-    cropped_all_images = np.zeros((img_count, IMG_SIZE, IMG_SIZE, 3))
+    cropped_all_images = np.zeros((img_count, IMG_HEIGHT, IMG_WIDTH, 3))
     
-    x_start = (ORIGINAL_IMG_SIZE - IMG_SIZE) // 2
-    y_start = ORIGINAL_IMG_SIZE * 3 // 8 - IMG_SIZE // 2
+    x_start = (ORIGINAL_IMG_SIZE - IMG_WIDTH) // 2
+    y_start = ORIGINAL_IMG_SIZE // 4
     
     for i in range(img_count):
-        cropped_all_images[i] = all_images[i][y_start : y_start+IMG_SIZE, x_start : x_start+IMG_SIZE]
+        cropped_all_images[i] = all_images[i][y_start : y_start+IMG_HEIGHT, x_start : x_start+IMG_WIDTH]
 
     return cropped_all_images
 
@@ -152,7 +157,7 @@ def load_training_data():
     # read output data
     eyes_male = read_output('regression_eyes_info_male.csv')
     eyes_female = read_output('regression_eyes_info_female.csv')
-    all_eyes_info = np.concatenate((mouth_male, mouth_female), axis=0)
+    all_eyes_info = np.concatenate((eyes_male, eyes_female), axis=0)
 
     # create dataset
     train_input_img = np.array(cropped_all_images)
@@ -171,7 +176,7 @@ def load_training_data():
 def is_converged_to_avg(cnn_model, N_imgs=15):
     train_input_img, train_input_gender_prob, _ = load_training_data() 
     
-    train_input_img_ = np.reshape(train_input_img[:N_imgs], (-1, IMG_SIZE * IMG_SIZE * 3))
+    train_input_img_ = np.reshape(train_input_img[:N_imgs], (-1, IMG_HEIGHT * IMG_WIDTH * 3))
     train_input_gender_prob_ = np.reshape(train_input_gender_prob[:N_imgs], (-1, 2))
 
     train_input = np.concatenate((train_input_img_, train_input_gender_prob_), axis=1)
