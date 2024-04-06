ce8850aec9eb03d78a19850aa55d524edfa2c434
wannabesuperteur <iwant_optimi@naver.com>
2024-04-07 00:33:19 +0900

    fix: decoder mock test RGB <-> BGR

--- Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/decoder_mock_test.py
+++ Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/decoder_mock_test.py
@@ -5,6 +5,11 @@ from PIL import Image
 HIDDEN_DIMS = 40
 
 
+# RGB <-> BGR
+def convert_RGB_to_BGR(original_image):
+    return original_image[:, :, ::-1]
+
+
 # 생성된 cvae_decoder_model 모의 테스트
 def mock_test_decoder(cvae_decoder):
     latent_space = np.random.normal(0.0, 1.0, size=(1, HIDDEN_DIMS))
@@ -13,10 +18,10 @@ def mock_test_decoder(cvae_decoder):
     img = cvae_decoder([latent_space, input_info])
     img_np = np.array(img.numpy() * 255.0, dtype=np.uint8)
 
-    print('result image :')
+    print('result image (B,G,R) :')
     print(img_np)
     
-    img_np_rgb = Image.fromarray(img_np[0])
+    img_np_rgb = Image.fromarray(convert_RGB_to_BGR(img_np[0]))
     img_np_rgb.save(f'test_image_mock_test.png')
 
 
