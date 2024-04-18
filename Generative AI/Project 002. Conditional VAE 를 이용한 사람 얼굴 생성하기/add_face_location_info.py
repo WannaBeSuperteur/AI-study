637ce2132c6102d8de4b00cd1818296ecd597a7d
wannabesuperteur <iwant_optimi@naver.com>
2024-04-18 22:21:58 +0900

    test: modify face location algorithm

--- Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/add_face_location_info.py
+++ Generative AI/Project 002. Conditional VAE 를 이용한 사람 얼굴 생성하기/add_face_location_info.py
@@ -8,12 +8,14 @@ IMG_CENTER = IMG_SIZE // 2
 
 CENTER_X = 60
 CENTER_Y = 68
-CENTER_RADIUS = 8
+CENTER_RADIUS = 12
 CENTER_PIXELS_COUNT = (2 * CENTER_RADIUS) * (2 * CENTER_RADIUS)
 EPSILON = 0.0001
 MASK_SAMPLES_PER_DATASET = 100
+MAX_PIXEL_DIST_BASE = 5500
 
 
+# (now unused) compute mean and std of center area pixel values
 def get_mean_and_std_of_center_pixels(center_pixels):
     center_pixels_ = np.reshape(center_pixels, (CENTER_PIXELS_COUNT, 3))
     center_pixels_rgb_ratio = [
@@ -28,37 +30,83 @@ def get_mean_and_std_of_center_pixels(center_pixels):
     return center_pixels_mean, center_pixels_std
 
 
-def create_mask(img, center_pixels_mean, center_pixels_std):
-    B, G, R = cv2.split(img)
-    ratio_bg = B / (G + EPSILON)
-    ratio_br = B / (R + EPSILON)
-    ratio_gr = G / (R + EPSILON)
+# get threshold distance for each position (y, x)
+def get_threshold_dist(y, x):
+    y_diff = max(0, abs(y - CENTER_Y) - 16)
+    x_diff = max(0, abs(x - CENTER_X) - 16)
 
-    mask_bg = abs((ratio_bg - center_pixels_mean[0]) / (center_pixels_std[0] + EPSILON)) <= 1.7
-    mask_br = abs((ratio_br - center_pixels_mean[1]) / (center_pixels_std[1] + EPSILON)) <= 1.7
-    mask_gr = abs((ratio_gr - center_pixels_mean[2]) / (center_pixels_std[2] + EPSILON)) <= 1.7
-    mask_all = mask_bg & mask_br & mask_gr
-    mask_all = mask_all.astype(np.uint8) * 255
+    return MAX_PIXEL_DIST_BASE / (y_diff + 12 * x_diff + 10)
+
+
+# distance of pixel
+def compute_dist(pixel0, pixel1):
+    pixel0_ = np.array(pixel0, dtype=np.int32)
+    pixel1_ = np.array(pixel1, dtype=np.int32)
+
+    blue_diff = (pixel0_[0] - pixel1_[0]) * (pixel0_[0] - pixel1_[0])
+    green_diff = (pixel0_[1] - pixel1_[1]) * (pixel0_[1] - pixel1_[1])
+    red_diff = (pixel0_[2] - pixel1_[2]) * (pixel0_[2] - pixel1_[2])
+
+    pixel_dist = blue_diff + green_diff + red_diff
+    return pixel_dist
+
+
+# run bfs to 'segment' face
+def run_bfs(mask_all, y, x, img):
+    cury = y
+    curx = x
+    curpixel = img[y][x]
+    dist_thresh = get_threshold_dist(y, x)
+    queue = []
+
+    while True:
+        for d in [1, 2]:
+            if cury >= d and mask_all[cury - d][curx] == 0 and compute_dist(curpixel, img[cury - d][curx]) <= dist_thresh:
+                mask_all[cury - d][curx] = 255
+                queue.append([cury - d, curx])
+
+            if cury < IMG_SIZE - d and mask_all[cury + d][curx] == 0 and compute_dist(curpixel, img[cury + d][curx]) <= dist_thresh:
+                mask_all[cury + d][curx] = 255
+                queue.append([cury + d, curx])
+
+            if curx >= d and mask_all[cury][curx - d] == 0 and compute_dist(curpixel, img[cury][curx - d]) <= dist_thresh:
+                mask_all[cury][curx - d] = 255
+                queue.append([cury, curx - d])
+
+            if curx < IMG_SIZE - d and mask_all[cury][curx + d] == 0 and compute_dist(curpixel, img[cury][curx + d]) <= dist_thresh:
+                mask_all[cury][curx + d] = 255
+                queue.append([cury, curx + d])
+
+        if len(queue) == 0:
+            return
+
+        cury, curx = queue.pop(0)
+        curpixel = img[cury][curx]
+        dist_thresh = get_threshold_dist(cury, curx)
+
+
+def create_mask(img):
+    mask_all = np.zeros((IMG_SIZE, IMG_SIZE))
+
+    for y in range(CENTER_Y - CENTER_RADIUS, CENTER_Y + CENTER_RADIUS):
+        for x in range(CENTER_X - CENTER_RADIUS, CENTER_X + CENTER_RADIUS):
+            mask_all[y][x] = 255
+            run_bfs(mask_all, y, x, img)
 
     return mask_all
 
 
-def first_consecutive_5_white_after_5_blacks(arr):
+def first_consecutive_2_whites(arr):
     arr_size = len(arr)
-    cur_blacks = 0
     cur_consecutive_whites = 0
 
     for i in range(arr_size):
-        if arr[i] == 0:
-            cur_blacks += 1
-
-        if cur_blacks >= 5:
-            if arr[i] > 0:
-                cur_consecutive_whites += 1
-                if cur_consecutive_whites >= 5:
-                    return i
-            else:
-                cur_consecutive_whites = 0
+        if arr[i] > 0:
+            cur_consecutive_whites += 1
+            if cur_consecutive_whites >= 2:
+                return i - 1
+        else:
+            cur_consecutive_whites = 0
 
     return arr_size
 
@@ -68,9 +116,9 @@ def get_face_location_info(img_mask):
     from_left = img_mask[IMG_CENTER]
     from_right = from_left[::-1]
 
-    face_loc_from_top = first_consecutive_5_white_after_5_blacks(from_top)
-    face_loc_from_left = first_consecutive_5_white_after_5_blacks(from_left)
-    face_loc_from_right = first_consecutive_5_white_after_5_blacks(from_right)
+    face_loc_from_top = first_consecutive_2_whites(from_top)
+    face_loc_from_left = first_consecutive_2_whites(from_left)
+    face_loc_from_right = first_consecutive_2_whites(from_right)
 
     return {
         'face_location_top': [face_loc_from_top],
@@ -83,12 +131,8 @@ def add_face_location_info(image_name, image_dir, face_location_info_df, face_lo
     img_path = image_dir + '/' + image_name
     img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED) # (120, 120, 3)
 
-    # distribution of central pixels (face)
-    center_pixels = img[CENTER_Y-CENTER_RADIUS : CENTER_Y+CENTER_RADIUS, CENTER_X-CENTER_RADIUS : CENTER_X+CENTER_RADIUS]
-    center_pixels_mean, center_pixels_std = get_mean_and_std_of_center_pixels(center_pixels)
-
     # create mask
-    img_mask = create_mask(img, center_pixels_mean, center_pixels_std)
+    img_mask = create_mask(img)
 
     # save mask
     if save_mask:
@@ -113,6 +157,9 @@ def save_face_location_info_for_all_images():
     # create face location info
     for image_dir in image_dirs:
         for idx, image_name in enumerate(os.listdir(image_dir)):
+            if idx % 100 == 0:
+                print(image_dir, idx)
+
             face_location_info = add_face_location_info(
                 image_name, image_dir,
                 face_location_info_df=face_location_info,
@@ -147,6 +194,12 @@ def create_sample_face_location_info_dir():
         pass
 
 
+# result :
+"""
+column face_location_top, top 10% = 38, bottom 5% = 7
+column face_location_left, top 10% = 42, bottom 5% = 21
+column face_location_right, top 10% = 41, bottom 5% = 22
+"""
 if __name__ == '__main__':
     create_sample_face_location_info_dir()
     save_face_location_info_for_all_images()
