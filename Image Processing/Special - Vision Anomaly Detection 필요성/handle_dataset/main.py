418ecefe1277420035acce76d872fb3f9b895cdc
wannabesuperteur <iwant_optimi@naver.com>
2025-04-01 22:27:30 +0900

    ADD: skeleton code

---
+++ Image Processing/Special - Vision Anomaly Detection 필요성/handle_dataset/main.py
@@ -0,0 +1,5 @@
+from tvt_split import resize_all_images, split_dataset_into_groups, copy_images
+
+
+if __name__ == '__main__':
+    pass
---
+++ Image Processing/Special - Vision Anomaly Detection 필요성/handle_dataset/tvt_split.py
@@ -0,0 +1,45 @@
+# 전체 데이터셋의 이미지 resize 실시
+# Create Date : 2025.04.01
+# Last Update Date : -
+
+# Arguments:
+# - dest_size (int) : resize 이후의 이미지 가로/세로 길이 (default: 512)
+
+# Returns:
+# - mvtec_dataset 디렉토리 안의 모든 이미지를 dest_size x dest_size 로 resize
+
+def resize_all_images(dest_size=512):
+    raise NotImplementedError
+
+
+# 원본 데이터셋의 데이터를 여러 그룹으로 분리하여 각 그룹에 해당하는 이미지 경로 반환
+# Create Date : 2025.04.01
+# Last Update Date : -
+
+# Arguments:
+# - dataset_type  (str)         : 'train' or 'test'
+# - dataset_class (str)         : 'normal' or 'abnormal'
+# - category      (list(str))   : 세부 카테고리 목록
+# - split_ratio   (list(float)) : 각 그룹에 할당할 이미지의 비율
+
+# Returns:
+# - image_paths_per_group (dict(list)) : 각 그룹에 해당하는 이미지의 리스트를 저장한 dict
+
+def split_dataset_into_groups(dataset_type, dataset_class, category, split_ratio):
+    return NotImplementedError
+
+
+# 각 그룹에 해당하는 이미지 경로의 이미지를 특정 디렉토리에 복사
+# Create Date : 2025.04.01
+# Last Update Date : -
+
+# Arguments:
+# - image_paths_per_group (dict(list)) : 각 그룹에 해당하는 이미지의 리스트를 저장한 dict
+# - dir_names_to_copy     (dict(str))  : 각 그룹에 해당하는 이미지를 복사할 경로
+#                                        (key 조합은 image_paths_per_group 의 key 조합과 동일)
+
+# Returns:
+# - 각 그룹에 해당하는 이미지를 dir_names_to_copy 가 가리키는 디렉토리에 복사
+
+def copy_images(image_paths_per_group, dir_names_to_copy):
+    return NotImplementedError
