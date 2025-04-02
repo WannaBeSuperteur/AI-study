cf8f5b0d067f1f6287b067264a015e5bb1c54adc
wannabesuperteur <iwant_optimi@naver.com>
2025-04-02 16:52:02 +0900

    REFACTOR: move functions to common

--- Image Processing/Special - Vision Anomaly Detection 필요성/run_experiment/test_numeric_performance.py
+++ Image Processing/Special - Vision Anomaly Detection 필요성/run_experiment/test_numeric_performance.py
@@ -1,4 +1,15 @@
 
+try:
+    from common import (get_glass_model, get_tinyvit_model,
+                        get_datasets,
+                        run_train_glass, run_train_tinyvit,
+                        run_test_glass, run_test_tinyvit)
+except:
+    from run_experiment.common import (get_glass_model, get_tinyvit_model,
+                                       get_datasets,
+                                       run_train_glass, run_train_tinyvit,
+                                       run_test_glass, run_test_tinyvit)
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -6,79 +17,25 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from handle_dataset.main import get_category_list
 
 
-# GLASS 모델 가져오기
-# Create Date : 2025.04.02
-# Last Update Date : -
-
-# Arguments:
-# - 없음
-
-# Returns:
-# - model (nn.Module) : 학습 및 성능 테스트에 사용할 GLASS 모델
-
-def get_glass_model():
-    raise NotImplementedError
-
-
-# 학습, 검증 및 테스트 데이터셋 정의
-# Create Date : 2025.04.02
-# Last Update Date : -
-
-# Arguments:
-# - category_name (str) : 카테고리 이름
-
-# Returns:
-# - train_dataset (Dataset) : 해당 카테고리의 학습 데이터셋
-# - valid_dataset (Dataset) : 해당 카테고리의 검증 데이터셋
-# - test_dataset  (Dataset) : 해당 카테고리의 테스트 데이터셋
-
-def get_datasets(category_name):
-    raise NotImplementedError
-
-
-# 모델 학습 실시
-# Create Date : 2025.04.02
-# Last Update Date : -
-
-# Arguments:
-# - model         (nn.Module) : 학습 및 성능 테스트에 사용할 GLASS 모델
-# - train_dataset (Dataset)   : 학습 데이터셋 (카테고리 별)
-# - valid_dataset (Dataset)   : 검증 데이터셋 (카테고리 별)
-
-# Returns:
-# - val_loss_list (list) : Valid Loss 기록
-
-def run_train(model, train_dataset, valid_dataset):
-    raise NotImplementedError
-
-
-# 모델 테스트 실시
-# Create Date : 2025.04.02
-# Last Update Date : -
-
-# Arguments:
-# - model        (nn.Module) : 학습 및 성능 테스트에 사용할 GLASS 모델
-# - test_dataset (Dataset)   : 테스트 데이터셋 (카테고리 별)
-
-# Returns:
-# - test_result      (dict)             : 테스트 성능 평가 결과
-#                                         {'accuracy': float, 'recall': float, 'precision': float, 'f1_score': float}
-# - confusion_matrix (Pandas DataFrame) : 테스트 성능 평가 시 생성된 Confusion Matrix
-
-def run_test(model, test_dataset):
-    raise NotImplementedError
-
-
 if __name__ == '__main__':
-    glass_model = get_glass_model()
     category_list = get_category_list()
 
     for category_name in category_list:
+        glass_model = get_glass_model()
+        tinyvit_model = get_tinyvit_model()
+
         train_dataset, valid_dataset, test_dataset = get_datasets(category_name)
-        run_train(glass_model, train_dataset, valid_dataset)
 
-        test_result, confusion_matrix = run_test(glass_model, test_dataset)
+        run_train_glass(glass_model, train_dataset, valid_dataset)
+        run_train_tinyvit(tinyvit_model, train_dataset, valid_dataset)
 
-        print(f'\n==== TEST RESULT ({category_name}) ====')
-        print(test_result)
-        print(confusion_matrix)
+        test_result_glass, confusion_matrix_glass = run_test_glass(glass_model, test_dataset)
+        test_result_tinyvit, confusion_matrix_tinyvit = run_test_tinyvit(tinyvit_model, test_dataset)
+
+        print(f'\n==== TEST RESULT ({category_name}) of GLASS ====')
+        print(test_result_glass)
+        print(confusion_matrix_glass)
+
+        print(f'\n==== TEST RESULT ({category_name}) of TinyViT ====')
+        print(test_result_tinyvit)
+        print(confusion_matrix_tinyvit)
