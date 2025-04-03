efa3c56440c0b04fd0e9cb5e105971bcbe4186ae
wannabesuperteur <iwant_optimi@naver.com>
2025-04-03 14:56:02 +0900

    ADD: image augmentation code

--- Image Processing/Special - Vision Anomaly Detection 필요성/handle_dataset/main.py
+++ Image Processing/Special - Vision Anomaly Detection 필요성/handle_dataset/main.py
@@ -1,5 +1,8 @@
 from tvt_split import resize_all_images, split_dataset_into_groups, split_dataset_into_groups_lac, copy_images, get_lac
 
+import numpy as np
+import cv2
+
 import os
 PROJECT_DIR_PATH = os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
@@ -182,13 +185,56 @@ def split_data_exp3_classify(category_list, lac_dict):
 # Last Update Date : -
 
 # Arguments:
-# - dir_name (str) : Augmentation 을 실시할 데이터셋 디렉토리 이름
+# - dir_name      (str)       : Augmentation 을 실시할 데이터셋 디렉토리 이름
+# - category_list (list(str)) : 세부 카테고리 목록
 
 # Returns:
 # - 데이터셋 디렉토리의 모든 sub-dataset 에 대해 Train Abnormal Data 가 90,180,270도 회전 Augmentation 으로 4배 증대
 
-def do_augmentation(dir_name):
-    raise NotImplementedError
+def do_augmentation(dir_name, category_list):
+    dataset_dir_path = f'{PROJECT_DIR_PATH}/{dir_name}'
+    train_abnormal_dirs = [f'{dataset_dir_path}/{category}/train/abnormal' for category in category_list]
+
+    for train_abnormal_dir in train_abnormal_dirs:
+        train_abnormal_imgs = os.listdir(train_abnormal_dir)
+        train_abnormal_imgs = list(filter(lambda x: x.endswith('.png'), train_abnormal_imgs))
+        train_abnormal_img_paths = [f'{train_abnormal_dir}/{img_name}' for img_name in train_abnormal_imgs]
+
+        for path in train_abnormal_img_paths:
+            augment_and_save_img(path)
+
+
+# 각 이미지에 대해 90,180,270도 회전 Data Augmentation 실시
+# Create Date : 2025.04.03
+# Last Update Date : -
+
+# Arguments:
+# - path (str) : 이미지 전체 경로
+
+# Returns:
+# - 해당 이미지에 대해 Train Abnormal Data 가 90,180,270도 회전 Augmentation 으로 4배 증대
+
+def augment_and_save_img(path):
+
+    # 이미지 읽기 시 한글 경로 처리
+    train_abnormal_img = cv2.imdecode(np.fromfile(path, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    train_abnormal_img_90 = cv2.rotate(train_abnormal_img, cv2.ROTATE_90_CLOCKWISE)
+    train_abnormal_img_180 = cv2.rotate(train_abnormal_img, cv2.ROTATE_180)
+    train_abnormal_img_270 = cv2.rotate(train_abnormal_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    augmented_imgs = [train_abnormal_img_90, train_abnormal_img_180, train_abnormal_img_270]
+    angles = [90, 180, 270]
+
+    for augemented_img, angle in zip(augmented_imgs, angles):
+
+        # 이미지 저장 시 한글 경로 처리
+        result, img_arr = cv2.imencode(ext='.png', img=augemented_img, params=[cv2.IMWRITE_PNG_COMPRESSION, 0])
+        augemented_img_save_path = path[:-4] + '_rotate_' + str(angle) + '.png'
+
+        if result:
+            with open(augemented_img_save_path, mode='w+b') as f:
+                img_arr.tofile(f)
 
 
 if __name__ == '__main__':
@@ -201,14 +247,15 @@ if __name__ == '__main__':
     print(f'LAC DICT:\n{lac_dict}')
 
     # execute ONLY AT FIRST TIME resizing image
-#    resize_all_images(img_dir='mvtec_dataset_512', dest_size=TINYVIT_IMG_SIZE)
-#    resize_all_images(img_dir='mvtec_dataset_256', dest_size=GLASS_IMG_SIZE)
+    resize_all_images(img_dir='mvtec_dataset_512', dest_size=TINYVIT_IMG_SIZE)
+    resize_all_images(img_dir='mvtec_dataset_256', dest_size=GLASS_IMG_SIZE)
 
     # experiment dataset setting
-#    split_data_exp1_anomaly(category_list)
-#    split_data_exp1_classify(category_list)
-#    split_data_exp3_classify(category_list, lac_dict)
+    split_data_exp1_anomaly(category_list)
+    split_data_exp1_classify(category_list)
+    split_data_exp3_classify(category_list, lac_dict)
 
     # data augmentation for training abnormal data
-    do_augmentation('mvtec_dataset_exp1_classify')
-    do_augmentation('mvtec_dataset_exp3_classify')
+    do_augmentation('mvtec_dataset_exp1_classify', category_list)
+    do_augmentation('mvtec_dataset_exp3_classify', category_list)
+
