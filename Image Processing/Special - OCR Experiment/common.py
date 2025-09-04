cb943b05684395f972f521ee58e69d19b67a678b
wannabesuperteur <iwant_optimi@naver.com>
2025-09-04 23:27:28 +0900

    FIX: test image transformation

--- Image Processing/Special - OCR Experiment/common.py
+++ Image Processing/Special - OCR Experiment/common.py
@@ -4,7 +4,7 @@ import os
 
 
 def save_tensor_as_image(tensor, dir_name, img_idx):
-    transformed_tensor = 0.5 * tensor + 0.25
+    transformed_tensor = 0.25 * tensor + 0.5
     os.makedirs(dir_name, exist_ok=True)
 
     img_path = f'{dir_name}/image_{img_idx:04d}.png'
