b9b8c0733ba60f6fc70b5d2dad28a17dd48f4225
wannabesuperteur <iwant_optimi@naver.com>
2026-02-22 17:56:58 +0900

    ADD: tool call

--- LangChain Study/LangChain Practice/main.py
+++ LangChain Study/LangChain Practice/main.py
@@ -1,9 +1,10 @@
 
-import os
+from tool_functions import calculate_date_, calculate_day_of_week_, calculate_date, calculate_day_of_week
+import torch
+import json
 
 from langchain.agents import create_agent
-import torch
-from langchain_huggingface import HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
 from transformers import AutoModelForCausalLM, pipeline, AutoTokenizer, AutoConfig, BitsAndBytesConfig
 
 ORIGINAL_MIDM_LLM_PATH = 'llm_fine_tuning/midm_original_llm'
@@ -61,6 +62,41 @@ def load_langchain_llm(llm_path: str):
     return langchain_llm
 
 
+def run_agent(agent, tools_original_functions):
+    """
+    Run LLM Agent.
+    Create Date: 2026.02.22
+
+    :param agent:                    LLM Agent to run
+    :param tools_original_functions: original tool function list
+    """
+
+    tool_map = {}
+    for tool in tools_original_functions:
+        tool_map[tool.__name__ + '_'] = tool
+
+    while True:
+        user_input = input('\nUSER INPUT:\n')
+        tool_result = agent.invoke({
+            "messages": [{'role': 'user', 'content': user_input}]
+        })
+        tool_result_msg = tool_result["messages"][-1]
+        tool_result_content = tool_result_msg.content.split('<|start_header_id|>assistant<|end_header_id|>\n\n')[-1]
+        tool_result_content_json = json.loads(tool_result_content)
+
+        try:
+            tool_call = tool_result_content_json["tool_call"]
+            tool_name = tool_call.get("name")
+            arg_dict = tool_call.get("arguments")
+            tool_execute_result = tool_map[tool_name](**arg_dict)
+
+            print('tool_execute_result :', tool_execute_result)
+
+        except Exception as e:
+            print(e)
+            tool_execute_result = '도구 호출 실패'
+
+
 if __name__ == '__main__':
 
     # BEFORE RUN:
@@ -74,5 +110,13 @@ if __name__ == '__main__':
     execute_tool_call_llm = load_langchain_llm('llm_fine_tuning/execute_tool_call_llm')
     final_output_llm = load_langchain_llm('llm_fine_tuning/final_output_llm')
 
-    import time
-    time.sleep(1000)
+    # Create and run LLM Agent
+    execute_tool_call_chat_llm = ChatHuggingFace(llm=execute_tool_call_llm)
+    tools = [calculate_date_, calculate_day_of_week_]
+    tools_original_functions = [calculate_date, calculate_day_of_week]
+
+    agent = create_agent(
+        model=execute_tool_call_chat_llm,
+        tools=tools
+    )
+    run_agent(agent, tools_original_functions)
